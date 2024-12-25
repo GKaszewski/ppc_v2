5fb26a41e2c7a9c201ae81b17766964f7ccd1846
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2024-12-25 17:02:34 +0100

    Flip player component

---
+++ scripts/components/flip_player.gd
@@ -0,0 +1,23 @@
+class_name FlipPlayerComponent
+extends Node2D
+
+@export var eye_left: Sprite2D
+@export var eye_right: Sprite2D
+@export var player_controller: PlayerController
+
+func _process(_delta: float) -> void:
+	var velocity = player_controller.velocity
+	if velocity.x < 0:
+		eye_left.frame = 1
+		eye_right.frame = 1
+		eye_left.flip_h = true
+		eye_right.flip_h = true
+	elif velocity.x > 0:
+		eye_left.frame = 1
+		eye_right.frame = 1
+		eye_left.flip_h = false
+		eye_right.flip_h = false
+	else:
+		eye_left.frame = 0
+		eye_right.frame = 0
+		
