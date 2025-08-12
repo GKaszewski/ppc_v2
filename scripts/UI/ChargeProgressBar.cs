407523193991031b7b27fe766c02b8ce72bcf9dc
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2025-08-13 00:30:37 +0200

    Add ChargeProgressBar, Credits, and GameOverScreen components for UI management

---
+++ scripts/UI/ChargeProgressBar.cs
@@ -0,0 +1,81 @@
+using Godot;
+using Mr.BrickAdventures.scripts.components;
+using Mr.BrickAdventures.scripts.Resources;
+
+namespace Mr.BrickAdventures.scripts.UI;
+
+public partial class ChargeProgressBar : Node
+{
+    [Export] public ProgressBar ProgressBar { get; set; }
+    [Export] public BrickThrowComponent ThrowComponent { get; set; }
+    
+    private ChargeThrowInputResource _throwInput;
+
+    public override void _Ready()
+    {
+        Owner.ChildEnteredTree += OnNodeEntered;   
+        ProgressBar.Hide();
+        SetupDependencies();
+    }
+
+    private void OnNodeEntered(Node node)
+    {
+        if (node is not BrickThrowComponent throwComponent || ThrowComponent != null) return;
+        ThrowComponent = throwComponent;
+        SetupDependencies();
+    }
+
+    private void SetupDependencies()
+    {
+        if (ThrowComponent.ThrowInputBehavior is ChargeThrowInputResource throwInput)
+        {
+            _throwInput = throwInput;
+        }
+        else
+        {
+            _throwInput = null;
+        }
+        
+        if (_throwInput == null)
+        {
+            return;
+        }
+
+        if (!_throwInput.SupportsCharging())
+        {
+            ProgressBar.Hide();
+            return;
+        }
+        
+        SetupProgressBar();
+        
+        _throwInput.ChargeStarted += OnChargeStarted;
+        _throwInput.ChargeStopped += OnChargeStopped;
+        _throwInput.ChargeUpdated += OnChargeUpdated;
+    }
+
+    private void SetupProgressBar()
+    {
+        ProgressBar.MinValue = _throwInput.MinPower;
+        ProgressBar.MaxValue = _throwInput.MaxPower;
+        ProgressBar.Value = _throwInput.MinPower;
+        ProgressBar.Step = 0.01f;
+        ProgressBar.Hide();
+    }
+
+    private void OnChargeStarted()
+    {
+        ProgressBar.Show();
+    }
+
+    private void OnChargeStopped()
+    {
+        ProgressBar.Hide();
+    }
+
+    private void OnChargeUpdated(float chargeRatio)
+    {
+        ProgressBar.Value = chargeRatio;
+        ProgressBar.Show();
+    }
+}
---
+++ scripts/UI/GameOverScreen.cs
@@ -0,0 +1,39 @@
+using Godot;
+using Mr.BrickAdventures.Autoloads;
+
+namespace Mr.BrickAdventures.scripts.UI;
+
+public partial class GameOverScreen : Node
+{
+    [Export] public Control GameOverPanel { get; set; }
+    [Export] public Button RestartButton { get; set; }
+    [Export] public Button MainMenuButton { get; set; }
+    [Export] public PackedScene MainMenuScene { get; set; }
+    
+    private GameManager _gameManager;
+
+    public override void _Ready()
+    {
+        _gameManager = GetNode<GameManager>("/root/GameManager");
+        RestartButton.Pressed += OnRestartClicked;
+        MainMenuButton.Pressed += OnMainMenuClicked;
+    }
+
+    private void OnMainMenuClicked()
+    {
+        _gameManager.ResetPlayerState();
+        GetTree().ChangeSceneToPacked(MainMenuScene);
+    }
+
+    private void OnRestartClicked()
+    {
+        _gameManager.RestartGame();
+    }
+
+    public void OnPlayerDeath()
+    {
+        if (_gameManager == null || _gameManager.GetLives() != 0) return;
+        
+        GameOverPanel.Show();
+    }
+}
